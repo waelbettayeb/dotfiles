242017d2a492cab26ee3d5641eabcbe54b6f7e75
Wael Bettayeb <bettayebwael@gmail.com>
2025-04-19 14:13:07 +0400

    wezterm keys

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,6 +1,7 @@
 local wezterm = require("wezterm")
 
-return {
+--- @type wezterm.Config
+config = {
 	colors = {},
 	window_padding = {
 		left = 0,
@@ -8,13 +9,115 @@ return {
 		top = 0,
 		bottom = 0,
 	},
+	hide_tab_bar_if_only_one_tab = true,
+	use_fancy_tab_bar = false,
+	native_macos_fullscreen_mode = true,
+	leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 },
 	keys = {
 		-- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
 		{ key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
 		-- Make Option-Right equivalent to Alt-f; forward-word
 		{ key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
+		-- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+		{
+			key = "a",
+			mods = "LEADER|CTRL",
+			action = wezterm.action.SendKey({ key = "a", mods = "CTRL" }),
+		},
+		{
+			key = "P",
+			mods = "LEADER|SHIFT",
+			action = wezterm.action.ActivateCommandPalette,
+		},
+		{
+			key = "%",
+			mods = "LEADER",
+			action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+		},
+		{
+			key = '"',
+			mods = "LEADER",
+			action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+		},
+		{
+			key = "x",
+			mods = "LEADER",
+			action = wezterm.action.CloseCurrentPane({ confirm = true }),
+		},
+		{
+			key = "z",
+			mods = "LEADER",
+			action = wezterm.action.TogglePaneZoomState,
+		},
+		-- Pane/Navigation
+		{
+			key = "h",
+			mods = "LEADER",
+			action = wezterm.action.ActivatePaneDirection("Left"),
+		},
+		{
+			key = "l",
+			mods = "LEADER",
+			action = wezterm.action.ActivatePaneDirection("Right"),
+		},
+		{
+			key = "k",
+			mods = "LEADER",
+			action = wezterm.action.ActivatePaneDirection("Up"),
+		},
+		{
+			key = "j",
+			mods = "LEADER",
+			action = wezterm.action.ActivatePaneDirection("Down"),
+		},
+		{
+			key = "LeftArrow",
+			mods = "LEADER",
+			action = wezterm.action.ActivatePaneDirection("Left"),
+		},
+		{
+			key = "RightArrow",
+			mods = "LEADER",
+			action = wezterm.action.ActivatePaneDirection("Right"),
+		},
+		{
+			key = "UpArrow",
+			mods = "LEADER",
+			action = wezterm.action.ActivatePaneDirection("Up"),
+		},
+		{
+			key = "DownArrow",
+			mods = "LEADER",
+			action = wezterm.action.ActivatePaneDirection("Down"),
+		},
+		{
+			key = "q",
+			mods = "LEADER",
+			action = wezterm.action.PaneSelect({
+				alphabet = "0123456789",
+			}),
+		},
+		--tabs
+		{
+			key = "!",
+			mods = "LEADER | SHIFT",
+			action = wezterm.action_callback(function(win, pane)
+				local tab, window = pane:move_to_new_tab()
+			end),
+		},
+		{
+			key = "|",
+			mods = "LEADER | SHIFT",
+			action = wezterm.action.ActivateLastTab,
+		},
 	},
-	hide_tab_bar_if_only_one_tab = true,
-	use_fancy_tab_bar = false,
-	native_macos_fullscreen_mode = true,
 }
+for i = 1, 8 do
+	-- CTRL+ALT + number to move to that position
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "LEADER",
+		action = wezterm.action.MoveTab(i - 1),
+	})
+end
+return config
