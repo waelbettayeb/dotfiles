3341abb082f3fd1be26acea928da7efe71e4fcf9
Wael Bettayeb <bettayebwael@gmail.com>
2025-06-29 11:38:45 +0400

    update wezterm conf

--- .wezterm.lua
+++ .wezterm.lua
@@ -13,8 +13,13 @@ config = {
 	hide_tab_bar_if_only_one_tab = true,
 	use_fancy_tab_bar = false,
 	native_macos_fullscreen_mode = true,
-	leader = { key = "`", mods = "CTRL", timeout_milliseconds = 1000 },
+	leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1500 },
 	keys = {
+		{
+			key = "r",
+			mods = "LEADER",
+			action = wezterm.action.ReloadConfiguration,
+		},
 		-- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
 		{ key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
 		-- Make Option-Right equivalent to Alt-f; forward-word
@@ -98,6 +103,19 @@ config = {
 				alphabet = "1234567890",
 			}),
 		},
+		{
+			key = "q",
+			mods = "LEADER|SHIFT",
+			action = wezterm.action.PaneSelect({
+				alphabet = "1234567890",
+				mode = "SwapWithActiveKeepFocus",
+			}),
+		},
+		-- resize panes
+		{ key = "H", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Left", 5 } }) },
+		{ key = "J", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Down", 5 } }) },
+		{ key = "K", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Up", 5 } }) },
+		{ key = "L", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Right", 5 } }) },
 		--tabs
 		{
 			key = "!",
@@ -111,6 +129,32 @@ config = {
 			mods = "LEADER | SHIFT",
 			action = wezterm.action.ActivateLastTab,
 		},
+		{ key = "&", mods = "LEADER|SHIFT", action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
+		-- rename window
+		{
+			key = ",", -- comma
+			mods = "LEADER", -- or use "CTRL" or "ALT" as you prefer
+			action = wezterm.action.PromptInputLine({
+				description = "Rename Tab",
+				action = wezterm.action_callback(function(window, pane, line)
+					if line then
+						window:active_tab():set_title(line)
+					end
+				end),
+			}),
+		},
+		-- tabs navigation
+		{ key = "p", mods = "LEADER", action = wezterm.action.ActivateTabRelative(-1) },
+		{ key = "n", mods = "LEADER", action = wezterm.action.ActivateTabRelative(1) },
+		{ key = "1", mods = "LEADER", action = wezterm.action({ ActivateTab = 0 }) },
+		{ key = "2", mods = "LEADER", action = wezterm.action({ ActivateTab = 1 }) },
+		{ key = "3", mods = "LEADER", action = wezterm.action({ ActivateTab = 2 }) },
+		{ key = "4", mods = "LEADER", action = wezterm.action({ ActivateTab = 3 }) },
+		{ key = "5", mods = "LEADER", action = wezterm.action({ ActivateTab = 4 }) },
+		{ key = "6", mods = "LEADER", action = wezterm.action({ ActivateTab = 5 }) },
+		{ key = "7", mods = "LEADER", action = wezterm.action({ ActivateTab = 6 }) },
+		{ key = "8", mods = "LEADER", action = wezterm.action({ ActivateTab = 7 }) },
+		{ key = "9", mods = "LEADER", action = wezterm.action({ ActivateTab = 8 }) },
 	},
 }
 for i = 1, 8 do
